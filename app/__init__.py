7bf2063cfc9e37ed15af4fb57bc5cfa8dd65ef9c
Jose Gabriel Jiménez <josegabojimenez@gmail.com>
2023-10-30 11:57:59 -0500

    feat: integrate api gateway to the delivery microservice

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from app.api.forkfiesta import forkfiesta_routes
 
 # Configuration
@@ -6,6 +7,7 @@ import app.config
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)  # Enable CORS for all routes
 
     # Register blueprints
     app.register_blueprint(forkfiesta_routes, url_prefix="/api/forkfiesta")
